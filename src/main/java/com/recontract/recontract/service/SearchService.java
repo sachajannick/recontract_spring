f7d651d26c60929565bf161f68a9883b58dfd3de
sachajannick <sachavanneck@gmail.com>
2021-09-28 03:42:14 +0200

    updated Search controller & service logic, added dto

--- src/main/java/com/recontract/recontract/service/SearchService.java
+++ src/main/java/com/recontract/recontract/service/SearchService.java
@@ -3,7 +3,6 @@ package com.recontract.recontract.service;
 import com.recontract.recontract.domain.Search;
 
 public interface SearchService {
-
     Search getSearchById(Long userId);
     void createSearch(Search search, Long userId);
     void updateSearch(String functionTitle, int amount, Long searchId);
